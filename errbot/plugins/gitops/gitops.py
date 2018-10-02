00b4a3a3b43ee73708547d81d856bdf633a732ba
Ederson Torresini <boidacarapreta@gmail.com>
2018-10-02 18:59:13 +0000

    Initial data model #5
    Collection "subscribers".

--- errbot/plugins/gitops/gitops.py
+++ errbot/plugins/gitops/gitops.py
@@ -1,7 +1,7 @@
 from errbot import BotPlugin, botcmd, webhook, ONLINE, AWAY
 from bottle import abort
-from requests import head, codes
 from pymongo import MongoClient
+from requests import head, codes
 
 class GitOps(BotPlugin):
     """Gitops integration: PubSub via Webhook"""
@@ -10,31 +10,16 @@ class GitOps(BotPlugin):
     def check_mongo(self):
         client = MongoClient()
         try:
-            response = client.chat0ps.command("ping")
+            response = client.local.command("ping")
         except:
-            self.change_presence(AWAY)
-            self.warn_admins("MongoDB down!")
-            self.stop_poller(self.check_mongo)
+            self.change_presence(AWAY, "MongoDB is down!")
         else:
-            self.change_presence(ONLINE)
+            self.change_presence(ONLINE, "All services up and running.")
 
 
     def activate(self):
         super().activate()
-        self.start_poller(10, self.check_mongo)
-
-
-    @botcmd(split_args_with=None)
-    def mongodb_up(self, msg, args):
-        """
-        Notify chatbot MongoDB is up and running.
-        """
-        if (self.check_mongo, [], {}) in self.current_pollers:
-            yield "Command ignored: bot already notified."
-        else:
-            yield "Restarting poller: MongoDB status check..."
-            self.start_poller(10, self.check_mongo)
-            yield "Done."
+        self.start_poller(60, self.check_mongo)
 
 
     @webhook(raw=True)
@@ -51,50 +36,76 @@ class GitOps(BotPlugin):
             # Soon others providers, like GitLab, will be available.
             abort(400, "Bad Request")
 
+
+    def validURL(self, args):
+        if len(args) == 0:
+            return None
+        url = args[0]
+        try:
+            response = head(url, allow_redirects=True)
+        except:
+            return None
+        if response.status_code == codes.ok:
+            return url
+        else:
+            return None
+
+
     @botcmd(split_args_with=None)
     def subscribe(self, msg, args):
         """
         Subscribe to repository notifications.
         It takes only one mandatory argument: the respository URL (must be public HTTP or HTTPS).
         """
-        if len(args) == 0:
-            yield "Please inform repository URL."
-            returnd
-        elif len(args) >= 2:
-            yield "Taking only the first argument as URL (discarding the rest...)."
-        url = args[0]
-        yield "Trying to reach site. Please wait a moment..."
-        try:
-            response = head(url)
-        except:
-            yield "Invalid URL. Please inform a valid one."
-            return
-        if response.status_code == codes.ok:
-            yield "Success! Subscribing to " + url + "..."
-            document = {
-            "repository": url,
-                "subscribers": [
-                    msg.frm.person
-                ]
-            }
-            client = MongoClient()
-            if client.chat0ps.subscriptions.find(document):
-                yield "Repository already subscribed."
+        # Validate arguments: only the first in URL valid format
+        url = self.validURL(args)
+        if url:
+            collection = MongoClient().chat0ps.subscriptions
+            # First check: if repository exists
+            repository = {"repository": url}
+            if collection.count_documents(repository) >= 1:
+                # Second check: if user is subscribed to this repository
+                subscription = {"repository": url, "subscribers": msg.frm.person}
+                subscriptions = collection.count_documents(subscription)
+                if subscriptions >= 1:
+                    # No need to update collection.
+                    yield "Repository already subscribed."
+                else:
+                    # Add user to subscribers list
+                    collection.update(repository, {"$push": {"subscribers": msg.frm.person}})
+                    yield "Done."
+                    yield "You may now set repository webhook to: http://35.198.17.35/publish"
             else:
-                client.chat0ps.subscriptions.insert_one(document)
+                # If the repository doesn't exists, it's time to create 
+                # and subscribe the user to it in a single command.
+                # Note a little difference here: "subscribers" ust be a list.
+                subscription = {"repository": url, "subscribers": [msg.frm.person]}
+                collection.insert_one(subscription)
                 yield "Done."
         else:
-            yield "Failure! HTTP response: " + str(response.status_code) + "."
+            yield "Please inform a valid URL."
 
 
     @botcmd(split_args_with=None)
-    def unsunscribe(self, msg, args):
+    def unsubscribe(self, msg, args):
         """
         Unsubscribe to repository notifications
         It takes only one mandatory argument: the repository URL.
         """
-        for arg in args:
-            yield arg
+        url = self.validURL(args)
+        if url:
+            collection = MongoClient().chat0ps.subscriptions
+            # Check: if subscription exists
+            repository = {"repository": url}
+            document = {"repository": url, "subscribers": msg.frm.person}
+            subscriptions = collection.count_documents(document)
+            if subscriptions >= 1:
+                collection.update(repository, {"$pop": {"subscribers": msg.frm.person}})
+                yield "Done"
+            else:
+                yield "Sorry, you're not subscribedto this repository."
+        else:
+            yield "Please inform a valid URL."
 
 
     @botcmd
@@ -102,9 +113,13 @@ class GitOps(BotPlugin):
         """
         List all repository subscriptions.
         """
-        client = MongoClient()
-        query = {
-            "subscribers": msg.frm.person
-        }
-        for subscription in client.chat0ps.subscriptions.find(query):
-            yield subscription['repository']
+        collection = MongoClient().chat0ps.subscriptions
+        document = {"subscribers": msg.frm.person}
+        subscriptions = collection.count_documents(document)
+        # Check if there is at least one subscription
+        if subscriptions >=1:
+            # Yes, there is. Time to list them all.
+            for subscription in collection.find(document):
+                yield subscription["repository"]
+        else:
+            yield "Sorry, no subscribed repository."
