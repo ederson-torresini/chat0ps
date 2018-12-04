5a7751fe1f90de766e9ee9f484b76cceb11525e0
Ederson Torresini <boidacarapreta@gmail.com>
2018-12-04 19:49:28 -0200

    Code formatting: Terraform

--- terraform/gcp.tf
+++ terraform/gcp.tf
@@ -1,21 +1,20 @@
 // Variables
-variable "gcp_project" { }
-variable "gcp_zone" { }
-variable "gke_username" { }
-variable "gke_password" { }
+variable "gcp_project" {}
 
+variable "gcp_zone" {}
+variable "gke_username" {}
+variable "gke_password" {}
 
 // Provider
 provider "google" {
   credentials = "${file("gcp.json")}"
-  project = "${var.gcp_project}"
-  zone = "${var.gcp_zone}"
+  project     = "${var.gcp_project}"
+  zone        = "${var.gcp_zone}"
 }
-
 
 // Resources
 resource "google_container_cluster" "chatops" {
-  name = "chatops"
+  name               = "chatops"
   initial_node_count = 3
 
   master_auth {
@@ -24,20 +23,7 @@ resource "google_container_cluster" "chatops" {
   }
 }
 
-
 // Outputs
 output "master_endpoint" {
   value = "${google_container_cluster.chatops.endpoint}"
-}
-
-output "client_certificate" {
-  value = "${google_container_cluster.chatops.master_auth.0.client_certificate}"
-}
-
-output "client_key" {
-  value = "${google_container_cluster.chatops.master_auth.0.client_key}"
-}
-
-output "cluster_ca_certificate" {
-  value = "${google_container_cluster.chatops.master_auth.0.cluster_ca_certificate}"
 }
